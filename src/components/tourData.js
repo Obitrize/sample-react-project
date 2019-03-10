010f83ed398171ff3d66b2748ba9a1095124e443
Otrize <oegekwu@gmail.com>
2019-03-10 03:14:47 +0100

    first commit for the project

---
+++ src/components/tourData.js
@@ -0,0 +1,50 @@
+export const tourData = [
+{
+    id: 1,
+    city: "butterfly",
+    img: "./img/butterfly.jpg",
+    name: "butterfly bridge tour",
+    info: 
+    "A city with so much"
+},
+{
+    id: 2,
+    city: "city",
+    img: "./img/city.jpg",
+    name: "city bridge tour",
+    info: 
+    "A beautiful city on lights"
+},
+{
+    id: 3,
+    city: "desert",
+    img: "./img/desert.jpg",
+    name: "desert bridge tour",
+    info: 
+    "A beautiful city on lights"
+},
+{
+    id: 4,
+    city: "flower",
+    img: "./img/flower.jpg",
+    name: "a flower bridge tour",
+    info: 
+    "A beautiful flower that lights up the environment"
+},
+{
+    id: 5,
+    city: "night",
+    img: "./img/night.jpg",
+    name: "a dark night with the reflection of the moon and stars",
+    info: 
+    "The night is dark and beautiful with the twinkling stars that makes the children want to play"
+},
+{
+    id: 6,
+    city: "sunset",
+    img: "./img/sunset.jpg",
+    name: "The rising sun",
+    info: 
+    "A sun that lights up the world"
+},
+];
